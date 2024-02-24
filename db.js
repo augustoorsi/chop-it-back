dace898b7d0a320ac7edb049849de6cf52b4665f
augusto <augusford@hotmail.com>
2024-02-24 18:54:53 -0300

    local db

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-// require('dotenv').config()
+require('dotenv').config()
 const {Sequelize} = require("sequelize")
 const URLModel = require("./models/URL")
 
-// console.log(process.env.DATABASE);
-// console.log(process.env.USER);
-// console.log(process.env.PASSWORD);
+console.log(process.env.DATABASE);
+console.log(process.env.USER);
+console.log(process.env.PASSWORD);
 
-// const DATABASE = process.env.DATABASE
-// const USER = process.env.USER
-// const PASSWORD = process.env.PASSWORD
+const DATABASE = process.env.DATABASE
+const USER = process.env.USER
+const PASSWORD = process.env.PASSWORD
 
-// const sequelize = new Sequelize(`postgres://${USER}:${PASSWORD}@localhost/${DATABASE}`) 
-const sequelize = new Sequelize("postgres://augusto:HjBO1ki5SkZricfFNbAc5QZ5XVKD1vPB@dpg-cnc01cla73kc73ej210g-a/chopit")
+const sequelize = new Sequelize(`postgres://${USER}:${PASSWORD}@localhost/${DATABASE}`) 
+
 
 sequelize.authenticate()
     .then(() => {
