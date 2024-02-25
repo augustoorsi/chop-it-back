92cc2c7199f54055ab494846847b4d2508873a72
augusto <augusford@hotmail.com>
2024-02-25 18:09:21 -0300

    db deploy

--- db.js
+++ db.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 const {Sequelize} = require("sequelize")
 const URLModel = require("./models/URL")
 
-console.log(process.env.DATABASE);
-console.log(process.env.USER);
-console.log(process.env.PASSWORD);
+// console.log(process.env.DATABASE);
+// console.log(process.env.USER);
+// console.log(process.env.PASSWORD);
 
 
-const DATABASE = process.env.DATABASE
-const USER = process.env.USER
-const PASSWORD = process.env.PASSWORD
+// const DATABASE = process.env.DATABASE
+// const USER = process.env.USER
+// const PASSWORD = process.env.PASSWORD
 
-const sequelize = new Sequelize(`postgres://${USER}:${PASSWORD}@localhost/${DATABASE}`) 
-
+// const sequelize = new Sequelize(`postgres://${USER}:${PASSWORD}@localhost/${DATABASE}`) 
+const sequelize = new Sequelize("postgres://chopit_vrw3_user:f8etXfjaIe426nk0GAsHNMIuQUFvwLjS@dpg-cndqmtmd3nmc738mc0tg-a.oregon-postgres.render.com/chopit_vrw3")
 
 sequelize.authenticate()
     .then(() => {
